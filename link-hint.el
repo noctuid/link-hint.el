6f9359941146286142f3a25875fa90856c97d9b7
noctuid <noct@openmailbox.org>
2015-12-28 16:48:08 -0500

    Add GPL notice

--- link-hint.el
+++ link-hint.el
@@ -6,6 +6,21 @@
 ;; Package-Requires: ((avy "0.3.0") (emacs "24.1"))
 ;; Version: 0.1
 
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This packages gives commands for operating on visible links with avy. It is
 ;; inspired by link-hinting from vim-like browsers and browser plugins such as
