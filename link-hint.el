e7c9416b0afef023afdb4cfc47dfd8e879702740
Mohsin Kaleem <mohkale@kisara.moe>
2020-07-11 21:53:38 +0100

    (bug): fix minor mode check was always false
    In `link-hint--var-valid-p` we're checking for active major or minor modes. For the
    major-mode check we simply compared against the `major-mode` variable, but for the
    minor mode check we were using the `bound-and-true-p` macro as a function. instead of
    checking whether a minor mode variable was active, we were checking whether a
    variable named `var` was. Which caused the condition to always be false.

--- link-hint.el
+++ link-hint.el
@@ -254,7 +254,8 @@ link-hint-NAME."
 (defun link-hint--var-valid-p (var)
   "Return t if VAR is bound and true or is the current major mode."
   (or (eq var major-mode)
-      (bound-and-true-p var)))
+      (and (boundp var)
+           (symbol-value var))))
 
 (defun link-hint--type-valid-p (type)
   "Return whether TYPE is a valid type for the current buffer.
